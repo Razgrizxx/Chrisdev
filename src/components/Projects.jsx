fd65b9a02920c033c04b16829a37e31f4c763c3a
Razgrizsx <razgrisss41@gmail.com>
2025-10-20 11:40:13 -0300

    protfolio update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -24,7 +24,7 @@ const Projects = () => {
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {projectsList.map((project, index) => (
-            <ProjectCard key={project.id} project={project} index={index} />
+            <ProjectCard key={index} project={project} index={index} />
           ))}
         </div>
 
--- src/utils/projectsList.js
+++ src/utils/projectsList.js
@@ -1,6 +1,84 @@
 export const projectsList = [
   {
-    id: 1,
+    title: "Flightpath",
+    description:
+      "Flightpath is a modern wellness and massage therapy platform designed to connect clients with certified tantric practitioners. The site provides information about available services and enables easy appointment booking through an intuitive interface and responsive design.",
+    technologies: ["ReactJs", "TailwindCSS", "TypeScript", "Gsap"],
+    githubUrl: "https://github.com/Razgrizxx/flightpath.git",
+    liveUrl: "https://flight-path-beige.vercel.app/",
+    image: "/projects/flightpath.png",
+  },
+  {
+    title: "TantraTouch",
+    description:
+      "TantraTouch is a modern wellness and massage therapy platform designed to connect clients with certified tantric practitioners. The site provides information about available services and enables easy appointment booking through an intuitive interface and responsive design.",
+    technologies: ["ReactJs", "TailwindCSS", "TypeScript", "Gsap"],
+    githubUrl: "https://github.com/Razgrizxx/Tantra.git",
+    liveUrl: "https://tantratouch.com",
+    image: "/projects/tantra.png",
+  },
+  {
+    title: "LinkWorlds",
+    description:
+      "LinkWorlds is an app that let the users create list of links to their social media or any other site that they want, it offers a variety of options to customize the links and the page",
+    technologies: ["ReactJs", "TailwindCSS", "TypeScript", "NestJs", "MongoDB"],
+    githubUrl: "https://github.com/quantumvflux/linkz-client",
+    liveUrl: "https://linkworlds.site",
+    image: "/projects/linkz.png",
+  },
+  {
+    title: "Set",
+    description:
+      "Set is a leading company with over 15 years of experience in the maintenance and sale of specialized equipment for label and packaging printing across Mexico and South America.",
+    technologies: ["ReactJs", "TailwindCSS", "TypeScript", "Gsap"],
+    githubUrl: "https://github.com/Razgrizxx/set",
+    liveUrl: "https://set-sigma.vercel.app/",
+    image: "/projects/set.png",
+  },
+  {
+    title: "Affini-T",
+    description:
+      "Affini-T is a company dedicated to advanced cell therapy research, focused on developing innovative T-cell treatments to target cancer and improve patient outcomes through cutting-edge biotechnology.",
+    technologies: ["ReactJs", "TailwindCSS", "TypeScript", "Gsap"],
+    githubUrl: "https://github.com/Razgrizxx/affini-t.git",
+    liveUrl: "https://affinit.netlify.app/",
+    image: "/projects/affini-t.png",
+  },
+
+  {
+    title: "UiTool",
+    description:
+      "UiTool is a no code app that let the users create web apps, posts, emails among other things. Then the user can share those with the world.",
+    technologies: [
+      "NextJS",
+      "TailwindCSS",
+      "TypeScript",
+      "NodeJS",
+      "Express",
+      "MongoDB",
+      "Drag and Drop",
+    ],
+    githubUrl: "",
+    liveUrl: "https://uitool-dev.vercel.app",
+    image: "/projects/UiTool.png",
+  },
+  {
+    title: "Dot Dager",
+    description:
+      "This is a page my team in Abstract and i made for the youtuber Dot Dager as part of a competition, it contains a good amount of technologies like 3d models and lighting",
+    technologies: [
+      "ReactJs",
+      "CSS",
+      "Tailwind",
+      "TypeScript",
+      "React three fiber",
+      "ThreeJs",
+    ],
+    githubUrl: "",
+    liveUrl: "https://dot-dager-pickles.netlify.app",
+    image: "/projects/dager.png",
+  },
+  {
     title: "SpartanVideo",
     description:
       "SpartanVideo is a video streaming application that allows users to create an account and watch videos. It is built using Nextjs and is deployed on Vercel.",
@@ -21,7 +99,6 @@ export const projectsList = [
     image: "/projects/spartanvideo.png",
   },
   {
-    id: 2,
     title: "Fitness",
     description:
       "Fitness is a web application is a template for a fitness app where people can read about the bussiness, contact the owners and register",
@@ -38,50 +115,5 @@ export const projectsList = [
     githubUrl: "https://github.com/Razgrizsx/Fitness_App",
     liveUrl: "https://fitness-app-tawny.vercel.app/",
     image: "/projects/fitness.PNG",
-  },
-  {
-    id: 3,
-    title: "UiTool",
-    description:
-      "UiTool is a no code app that let the users create web apps, posts, emails among other things. Then the user can share those with the world.",
-    technologies: [
-      "NextJS",
-      "TailwindCSS",
-      "TypeScript",
-      "NodeJS",
-      "Express",
-      "MongoDB",
-      "Drag and Drop",
-    ],
-    githubUrl: "",
-    liveUrl: "https://uitool-dev.vercel.app",
-    image: "/projects/UiTool.png",
-  },
-  {
-    id: 4,
-    title: "Dot Dager",
-    description:
-      "This is a page my team in Abstract and i made for the youtuber Dot Dager as part of a competition, it contains a good amount of technologies like 3d models and lighting",
-    technologies: [
-      "ReactJs",
-      "CSS",
-      "Tailwind",
-      "TypeScript",
-      "React three fiber",
-      "ThreeJs",
-    ],
-    githubUrl: "",
-    liveUrl: "https://dot-dager-pickles.netlify.app",
-    image: "/projects/dager.png",
-  },
-  {
-    id: 5,
-    title: "LinkZ",
-    description:
-      "LinkZ is an app that let the users create list of links to their social media or any other site that they want, it offers a variety of options to customize the links (currently under development)",
-    technologies: ["ReactJs", "TailwindCSS", "TypeScript", "NestJs", "MongoDB"],
-    githubUrl: "",
-    liveUrl: "https://linkz-client.vercel.app/auth",
-    image: "/projects/linkz.png",
   },
 ];
